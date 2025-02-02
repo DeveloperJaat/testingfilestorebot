26978b73797b46e12c18e6035dd3a5ab85a9dbeb
DeveloperJaat <97823189+DeveloperJaat@users.noreply.github.com>
2025-02-02 15:21:28 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -382,7 +382,7 @@ async def get_shortlink(link):
     if "http" == https:
         https = "https"
         link = link.replace("http", https)
-    url = f'https://vnshortener.com/api'
+    url = f'https://inshorturl.com/api'
     params = {'api': URL_SHORTNER_WEBSITE_API,
               'url': link,
               }
