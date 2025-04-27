55ba1cf322746e5a8ddd7610482503b06c65766c
DeveloperJaat <97823189+DeveloperJaat@users.noreply.github.com>
2025-04-27 07:53:22 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -110,7 +110,7 @@ async def next_page(bot, query):
         [
            
             #InlineKeyboardButton(text="⚡ʜᴏᴡ ᴛᴏ ᴅᴏᴡɴʟᴏᴀᴅ⚡", url='https://t.me/show_time_network/7'),
-            InlineKeyboardButton(text="🔞Adult Channel🔞", url='https://t.me/+LHC6x0UsmmU3YWVl')
+            InlineKeyboardButton(text="🔞Adult Channel🔞", url='https://t.me/+n2JK67S5lW85YmI1')
         ]
     )
 
@@ -691,7 +691,7 @@ async def auto_filter(client, msg, spoll=False):
     btn.insert(0,
         [
            # InlineKeyboardButton(text="⚡ʜᴏᴡ ᴛᴏ ᴅᴏᴡɴʟᴏᴀᴅ⚡", url='https://t.me/show_time_network/7'),
-            InlineKeyboardButton(text="🔞Adult Channel🔞", url='https://t.me/+LHC6x0UsmmU3YWVl')
+            InlineKeyboardButton(text="🔞Adult Channel🔞", url='https://t.me/+n2JK67S5lW85YmI1')
         ]
     )
 
